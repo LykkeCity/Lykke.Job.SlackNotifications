d745ccbd8444783e3765456e8094386846cf7e86
Sergey Nesterov <sergey.nesterov@lykke.com>
2017-10-10 18:30:46 +0300

    Added env info to webhook not found message

--- src/Lykke.Job.SlackNotifications.Services/SrvSlackNotifications.cs
+++ src/Lykke.Job.SlackNotifications.Services/SrvSlackNotifications.cs
@@ -9,11 +9,18 @@ namespace Lykke.Job.SlackNotifications.Services
 {
     public class SrvSlackNotifications : ISlackNotificationSender
     {
+        private const string _unknownSender = "unknown sender";
+        private const string _unknownEnv = "Unknown env";
+        private const string _warning = "Warning";
+
         private readonly SlackSettings _settings;
+        private readonly string _environment;
 
         public SrvSlackNotifications(SlackSettings settings)
         {
             _settings = settings;
+            if (!string.IsNullOrEmpty(_settings.Env))
+                _environment = $"Environment: {_settings.Env}";
         }
 
         public async Task SendNotificationAsync(string type, string message, string sender = null)
@@ -21,22 +28,25 @@ namespace Lykke.Job.SlackNotifications.Services
             var webHookUrl = _settings.Channels.FirstOrDefault(x => x.Type == type)?.WebHookUrl;
             if (webHookUrl == null)
             {
-                webHookUrl = _settings.Channels.FirstOrDefault(x => x.Type == "Warning")?.WebHookUrl;
+                webHookUrl = _settings.Channels.FirstOrDefault(x => x.Type == _warning)?.WebHookUrl;
                 if (webHookUrl == null)
-                    webHookUrl = _settings.Channels.FirstOrDefault(x => x.Type.StartsWith("Warning"))?.WebHookUrl;
+                    webHookUrl = _settings.Channels.FirstOrDefault(x => x.Type.StartsWith(_warning))?.WebHookUrl;
                 if (webHookUrl != null)
                 {
                     if (string.IsNullOrWhiteSpace(sender))
-                        sender = "unknown sender";
+                        sender = _unknownSender;
+                    string env = _unknownEnv;
+                    if (!string.IsNullOrEmpty(_settings.Env))
+                        env = _settings.Env;
                     await HttpRequestClient.PostRequest(
-                        new { text = $"Couldn't find webhook for {type} from {sender}" }.ToJson(), webHookUrl);
+                        new { text = $"{env}: Couldn't find webhook for {type} from {sender}" }.ToJson(), webHookUrl);
                 }
                 return;
             }
 
             var strBuilder = new StringBuilder();
-            if (!string.IsNullOrEmpty(_settings.Env))
-                strBuilder.AppendLine($"Environment: {_settings.Env}");
+            if (_environment != null)
+                strBuilder.AppendLine(_environment);
             strBuilder.AppendLine(sender != null ? $"{sender} : {message}" : message);
 
             await HttpRequestClient.PostRequest(new { text = strBuilder.ToString() }.ToJson(), webHookUrl);
