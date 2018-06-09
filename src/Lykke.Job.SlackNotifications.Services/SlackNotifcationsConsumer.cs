4c25e4ab036d4c15a1e65071c7cdd2c76808ea54
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-06-09 16:17:51 +0300

    Added processing of multiple queues

--- src/Lykke.Job.SlackNotifications.Services/SlackNotifcationsConsumer.cs
+++ src/Lykke.Job.SlackNotifications.Services/SlackNotifcationsConsumer.cs
@@ -16,9 +16,6 @@ namespace Lykke.Job.SlackNotifications.Services
         private readonly INotificationFilter _notificationFilter;
         private readonly ILog _log;
 
-        private DateTime _lastSendTime = DateTime.MinValue;
-        private SlackNotificationRequestMsg _lastMsg;
-
         public SlackNotifcationsConsumer(
             ISlackNotificationSender srvSlackNotifications,
             INotificationFilter notificationFilter,
@@ -31,7 +28,68 @@ namespace Lykke.Job.SlackNotifications.Services
 
         [UsedImplicitly]
         [QueueTrigger("slack-notifications", timeoutInSeconds: 100)]
-        public async Task ProcessInMessage(SlackNotificationRequestMsg msg)
+        public Task ProcessGeneralMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications");
+        }
+
+        [UsedImplicitly]
+        [QueueTrigger("slack-notifications-critical", timeoutInSeconds: 100)]
+        public Task ProcessCriticalMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications-critical");
+        }
+
+        [UsedImplicitly]
+        [QueueTrigger("slack-notifications-debug", timeoutInSeconds: 100)]
+        public Task ProcessDebugMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications-debug");
+        }
+
+        [UsedImplicitly]
+        [QueueTrigger("slack-notifications-error", timeoutInSeconds: 100)]
+        public Task ProcessErrorMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications-error");
+        }
+
+        [UsedImplicitly]
+        [QueueTrigger("slack-notifications-information", timeoutInSeconds: 100)]
+        public Task ProcessInfoMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications-information");
+        }
+
+        [UsedImplicitly]
+        [QueueTrigger("slack-notifications-monitor", timeoutInSeconds: 100)]
+        public Task ProcessMonitorMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications-monitor");
+        }
+
+        [UsedImplicitly]
+        [QueueTrigger("slack-notifications-poison", timeoutInSeconds: 100)]
+        public Task ProcessPoisonMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications-poison");
+        }
+
+        [UsedImplicitly]
+        [QueueTrigger("slack-notifications-trace", timeoutInSeconds: 100)]
+        public Task ProcessTraceMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications-trace");
+        }
+
+        [UsedImplicitly]
+        [QueueTrigger("slack-notifications-warning", timeoutInSeconds: 100)]
+        public Task ProcessWarningMessage(SlackNotificationRequestMsg msg)
+        {
+            return ProcessMessageFromQueue(msg, "slack-notifications-warning");
+        }
+
+        private async Task ProcessMessageFromQueue(SlackNotificationRequestMsg msg, string queueName)
         {
             try
             {
@@ -40,19 +98,16 @@ namespace Lykke.Job.SlackNotifications.Services
                 if (muteItem != null && string.IsNullOrEmpty(muteItem.Type))
                 {
                     muteItem.MutedMessagesCount++;
-                    
+
                     if (muteItem.MutedMessagesCount > 1)
                         return;
 
-                    await _srvSlackNotifications.SendNotificationAsync(msg.Type, $"*[This message is muted for {muteItem.TimeToMute.TotalMinutes} minute(s)! (using: '{muteItem.Value}')]*: {msg.Message}", $"*[:tired_face: muted :tired_face: ]* {msg.Sender}");
+                    await _srvSlackNotifications.SendNotificationAsync(
+                        msg.Type,
+                        $"*[This message is muted for {muteItem.TimeToMute.TotalMinutes} minute(s)! (using: '{muteItem.Value}')]*: {msg.Message}",
+                        $"*[:tired_face: muted :tired_face: ]* {msg.Sender}");
                     return;
                 }
-
-                var now = DateTime.UtcNow;
-                if (_lastMsg != null && _lastMsg.Type == msg.Type
-                    && _lastMsg.Sender == msg.Sender && _lastMsg.Message == msg.Message
-                    && now.Subtract(_lastSendTime).TotalMinutes < 1)
-                    return;
 
                 string message = msg.Message;
 
@@ -60,13 +115,10 @@ namespace Lykke.Job.SlackNotifications.Services
                     message = $"[redirected from {msg.Type}] {message}";
 
                 await _srvSlackNotifications.SendNotificationAsync(muteItem?.Type ?? msg.Type, message, msg.Sender);
-
-                _lastMsg = msg;
-                _lastSendTime = now;
             }
             catch (Exception ex)
             {
-                await _log.WriteErrorAsync("SlackNotificationRequestsConsumer", "ProcessInMessage", msg.ToJson(), ex);
+                _log.WriteError($"ProcessMessageFromQueue: {queueName}", msg.ToJson(), ex);
                 throw;
             }
         }
